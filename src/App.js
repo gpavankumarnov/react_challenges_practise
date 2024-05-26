6c632f303e13fa0d91db43e1c6583bd9a5b306ee
gpavankumarnov <90756309+gpavankumarnov@users.noreply.github.com>
2024-05-26 11:24:03 +0530

    new App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { lazy } from "react";
 import RecieveAPIComp from "./components/FetchAPI/RecieveAPIComp";
-// import { store } from "./pages/ShoppingApp/Redux/store";
-// import { Provider } from "react-redux";
-// import { Suspense } from "react";
-// import FormData from "./components/React_Challenges_Website/FormData/FormData";
+
 
 // import StopWatch from "./components/React_Challenges_Website/StopwatchTimer/StopWatch";
 // import ProgressBar from "./components/React_Challenges_Website/ProgressBar/ProgressBar";
